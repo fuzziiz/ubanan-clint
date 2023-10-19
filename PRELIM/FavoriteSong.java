e68e66882c129e8d887666cfbaceb1781135dc40
unknown <OO344@ccs.local>
2023-10-20 01:15:54 +0800

    updating PRELIM folder

---
+++ PRELIM/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+   public static void main(String[] args) {
+      System.out.println("Tatlong bilyon, ikaw lamang ang aking gusto");
+      System.out.println("Pasensya na kung ngayon ako'y 'di para sa'yo");
+      System.out.println("Tayo ay papunta na sa'ting bagong yugto");
+      System.out.println("'Yokong mabuhay sa isang mundong walang tayo");
+   }
+}
